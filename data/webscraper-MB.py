314c895e77032b02bf475274a4dad6dd99288bf2
Alice Huang <aliceshuang3@gmail.com>
2020-06-28 22:50:56 -0500

    finish scraping sites

--- data/webscraper-MB.py
+++ data/webscraper-MB.py
@@ -10,54 +10,83 @@ recipes = []
 for i in range(1, 64):
     # update page url
     url = 'https://minimalistbaker.com/recipe-index/?fwp_paged=' + str(i)
-    response = requests.get(url, timeout=5)
-    content = BeautifulSoup(response.content, "html.parser")
+    try:
+        response = requests.get(url, timeout=5)
+    except requests.exceptions.Timeout:
+        print("Timeout occurred")
 
-    # scrape feed listings
-    feed = content.findAll('h3', attrs={"class": "post-summary__title"})
-    for item in feed:
-        if "Matcha" in item.text:
+    # check if valid url - response returned status code 200
+    if response.ok:
+        content = BeautifulSoup(response.content, "html.parser")
+
+        # scrape feed listings on that page
+        feed = content.findAll('h3', attrs={"class": "post-summary__title"})
+        for item in feed:
             recipes.append(item.text)
 
-print(recipes)
-
-# loop through all matcha recipes on feed
+# loop through all recipes
 allRecipes = []
 
 for food in recipes:
-    recipe1 = food
     # get rid of parentheses, punctuation, in title for url
-    newRecipe1 = recipe1.lower().replace(" ", "-").replace("(", "").replace(")", "").replace("!", "")
+    newRecipe1 = food.lower().replace(" ", "-").replace("(", "").replace(")", "").replace("!", "")
 
     # form new recipe-specific url
     url = 'https://minimalistbaker.com/' + newRecipe1
-    response = requests.get(url, timeout=5)
-    content = BeautifulSoup(response.content, "html.parser")
+    try:
+        response = requests.get(url, timeout=5)
+    except requests.exceptions.Timeout:
+        print("Timeout occurred")
 
-    # scrape only if page is individual recipe
-    if content.find('li', attrs={"class": "wprm-recipe-ingredient"}):
-        # scrape recipe name
-        name = content.find('h1', attrs={"class": "entry-title"}).text
+    if response.ok:
+        content = BeautifulSoup(response.content, "html.parser")
 
-        # scrape servings
-        servings = content.find('span', attrs={"class": "wprm-recipe-servings-with-unit"}).text
+        # scrape only if page is individual recipe
+        # compilation recipes all show up individually in feed anyway
+        if content.find('li', attrs={"class": "wprm-recipe-ingredient"}):
+            # scrape recipe name
+            if content.find('h1', attrs={"class": "entry-title"}):
+                name = content.find('h1', attrs={"class": "entry-title"}).text
+            else:
+                # some pages don't have a title up top?
+                name = content.find('h2', attrs={"class": "wprm-recipe-name"}).text
 
-        # scrape ingredients list
-        ingredList = []
-        for ingredient in content.findAll('li', attrs={"class": "wprm-recipe-ingredient"}):
-            ingredList.append(ingredient.text)
+            # scrape servings
+            try:
+                servings = content.find('span', attrs={"class": "wprm-recipe-servings-with-unit"}).text + "servings"
+            except:
+                servings = "NA"
+                print("No servings data found")
 
-        # convert scraped data to json
-        recipeObject = {
-            "name": name,
-            "servings": servings,
-            "ingredients": ingredList
-        }
+            # scrape ingredients list
+            ingredList = []
+            for ingredient in content.findAll('li', attrs={"class": "wprm-recipe-ingredient"}):
+                ingredList.append(ingredient.text)
 
-        print(recipeObject)
+            # scrape instructions
+            instructions = []
+            for step in content.findAll('div', attrs={"class": "wprm-recipe-instruction-text"}):
+                instructions.append(step.text)
 
-        allRecipes.append(recipeObject)
+            # scrape prep time/cook time?
 
-# save data in json file
-with open('MBData.json', 'w') as outfile:
-    json.dump(allRecipes, outfile)
+            # "scrape link" - url above
+
+            # scrape image
+            img = content.find('img', attrs={"class": "attachment-thumbnail"}).get("src")
+
+            # convert scraped data to json
+            recipeObject = {
+                "name": name,
+                "servings": servings,
+                "ingredients": ingredList,
+                "instructions": instructions,
+                "link": url,
+                "image": img
+            }
+
+            allRecipes.append(recipeObject)
+
+# save data in json file after all recipes scraped
+with open('data/MBData.json', 'w', encoding='utf-8') as outfile:
+    json.dump(allRecipes, outfile, ensure_ascii=False, indent=4)
--- data/webscraper.py
+++ data/webscraper.py
@@ -48,12 +48,16 @@ def getRecipeDetails(response, url, allRecipes):
 
 
 # helper function
+# find all individual recipes on 1 "page" of the site: before the user has
+# to click "Show More" at the bottom to get to the next page of recipes
 def getRecipesOnPage(recipe, allRecipes):
     name = recipe['name'].strip()
     converted_name = name.lower().replace(" ", "-")
-    if recipe['content_type'] == "recipe":
-        # single recipe
 
+    # check if single recipe or compilation
+    if recipe['content_type'] == "recipe":
+        # single recipe - make GET request to that recipe's url, call function
+        # to scrape its page
         test_link = 'https://tasty.co/recipe/' + converted_name
         try:
             response = requests.get(test_link, timeout=10)
@@ -69,8 +73,11 @@ def getRecipesOnPage(recipe, allRecipes):
         except requests.exceptions.Timeout:
             print("Timeout occurred")
         if response.ok:
+            # get all the individual recipes on the compilation page
             content = BeautifulSoup(response.content, "html.parser")
             feed = content.findAll('a', attrs={"class": "feed-item"})
+            # for each recipe, make GET request to that recipe's url, call function
+            # to scrape its page
             for item in feed:
                 test_link = item.get("href")
                 try:
@@ -93,10 +100,14 @@ def getSubmenuRecipes(submenu, allRecipes):
     except requests.exceptions.Timeout:
         print("Timeout occurred")
 
+    # makes response readable json - returns 3 things: items (recipes), page (current page #), next (url of next page)
     all_json = response.json()
+    # go through each item on page, call function to figure out if they are
+    # individual recipes or compilations
     for recipe in all_json["items"]:
         getRecipesOnPage(recipe, allRecipes)
     # subsequent pages - continue as long as their is a next page (not None)
+    # keep making GET requests and parsing the json returned
     while all_json["next"]:
         url = 'https://tasty.co' + all_json["next"]
         try:
@@ -110,9 +121,14 @@ def getSubmenuRecipes(submenu, allRecipes):
 
 
 # ------------------MAIN PROGRAM---------------------------------------------
+# init list for all json objects for all recipes
 allRecipes = []
+# scrape each submenu under Recipes on Tasty's homepage
+# scrape "easy" recipes to fit our app theme
 getSubmenuRecipes("no_bake_desserts", allRecipes)
+getSubmenuRecipes("5_ingredients_or_less", allRecipes)
+getSubmenuRecipes("meal_prep", allRecipes)
 
-# save data in json file
+# save data in json file at the end after all recipes are scraped 
 with open('data/tastyData.json', 'w', encoding='utf-8') as outfile:
     json.dump(allRecipes, outfile, ensure_ascii=False, indent=4)
