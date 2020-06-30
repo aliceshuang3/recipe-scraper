533192807996c23da9185c54f0f059dbab35bc39
JacquelynChow <icecreamjackie@gmail.com>
2020-06-30 16:10:20 -0400

    styled alert and home background/button and other responsive design aspects

--- app/forms.py
+++ app/forms.py
@@ -33,7 +33,6 @@ class SignUpForm(FlaskForm):
 
 class ResetRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
@@ -43,4 +42,3 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
