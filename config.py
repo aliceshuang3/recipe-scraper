ee1a08f1c57c15d037868beab7417a1f08f495a6
Jacquelyn Chow <icecreamjackie@gmail.com>
2020-08-20 12:19:06 -0400

    config py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # The first term looks for the value of an environment variable, also called SECRET_KEY. 
     # The second term, is just a hardcoded string
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 
         'sqlite:///' + os.path.join(basedir, 'app.db')
     # Suppress deprecation warning   
     SQLALCHEMY_TRACK_MODIFICATIONS = False
