975a3b72d4cc779501d42b38d3ce38b002fc72e9
Alice Huang <ahuang2@swarthmore.edu>
2020-08-25 02:37:15 +0000

    fix db pythonanywhere

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    # The first term looks for the value of an environment variable, also called SECRET_KEY. 
+    # The first term looks for the value of an environment variable, also called SECRET_KEY.
     # The second term, is just a hardcoded string
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    # Suppress deprecation warning   
+        'sqlite:////home/novicechef/recipe-scraper/app.db'
+    # Suppress deprecation warning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
